a23b2fdb1287409792e0d7a2b1421c1a6b7c1eb2
Матрехина Татьяна <tatiana.matrekhina@gmail.com>
2019-10-29 00:56:47 +0300

    Добавлен класс App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,49 @@
+import {PureComponent} from 'react';
 import * as React from 'react';
 import {WelcomeScreen} from '../welcome-screen/welcome-screen.jsx';
+import PropTypes from "prop-types";
 
-const settings = {
-  gameTime: 10,
-  errorCount: 2,
-  onClickStartButton: (() => {})
-};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return <WelcomeScreen
-    gameTime={settings.gameTime}
-    errorCount={settings.errorCount}
-    onClickStartButton={settings.onClickStartButton}
-  />;
+    this.state = {
+      question: -1,
+    };
+  }
+
+  render() {
+    const {
+      gameTime,
+      errorCount,
+      onClickStartButton,
+      questions,
+    } = this.props;
+
+    return <WelcomeScreen
+      gameTime={gameTime}
+      errorCount={errorCount}
+      onClickStartButton={onClickStartButton}
+    />;
+  }
+}
+
+App.propTypes = {
+  gameTime: PropTypes.number.isRequired,
+  errorCount: PropTypes.number.isRequired,
+  onClickStartButton: PropTypes.func,
+  questions: PropTypes.arrayOf(
+      PropTypes.shape({
+        type: PropTypes.string.isRequired,
+        genre: PropTypes.number.isRequired,
+        answers: PropTypes.arrayOf(
+            PropTypes.shape({
+              src: PropTypes.string.isRequired,
+              genre: PropTypes.string.isRequired,
+            }).isRequired
+        ).isRequired,
+      }).isRequired
+  ).isRequired,
 };
 
 export {App};
---
+++ src/mocks/questions.js
@@ -0,0 +1,26 @@
+const questions = [
+  {
+    type: `genre`,
+    genre: `rock`,
+    answers: [
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `rock`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `pop`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `jazz`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `rock`,
+      },
+    ],
+  },
+];
+
+export {questions};
